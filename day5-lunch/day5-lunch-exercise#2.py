b6e2e4664a95ec15ac300278ecfcbd7ea70a624a
qq961112 <jzhan235@jhu.edu>
2019-08-30 14:49:43 -0400

    day5-lunch exercises

---
+++ day5-lunch/day5-lunch-exercise#2.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+"""
+Usage: ./day5-lunch_exercise#2.py t_data.ctab
+    
+
+"""
+
+import sys
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+# mamba install ucsc-bigwigaverageoverbed
+
+new_df = []
+for i, line in enumerate(open(sys.argv[1])):
+    if i == 0:
+        continue
+    fields = line.rstrip("\n").split("\t")
+    chrom = str(fields[1])
+    strand = fields[2]
+    start = int(fields[3])
+    end = int(fields[4])
+    t_name = fields[5]
+    if strand == "+":
+        if start - 500 >= 0:
+            prostart = start - 500
+            proend = start + 500
+        else:
+            proend = start + 500
+            prostart = 0
+        new_df.append([chrom, prostart, proend, t_name])
+            
+    else:
+        if end - 500 >= 0:
+            prostart = end + 500
+            proend = end - 500
+        else:
+            prostart = end + 500
+            proend = 0
+        new_df.append([chrom, prostart, proend, t_name])
+    
+
+# print(len(new_df))
+
+for i in range(len(new_df)):
+    g = new_df[i]
+    # print(get_new_df)
+#     print(type(get_new_df))
+    # print(g[0],g[1],g[2],g[3],sep ="\t")
+        
+
+    for j in range(len(g)):
+        if j < len(g) - 1:
+            print(g[j],end = "\t")
+        else:
+            print(g[j])
+    # print('\n')
---
+++ day5-lunch/day5-lunch-exercise#4.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+"""
+Usage: ./day5-lunch-exercise#4.py H3K4me1.tab H3K4me3.tab H3K9me3.tab
+    
+
+"""
+
+import sys
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import scipy
+
+
+df1 = pd.read_csv(sys.argv[1],sep = "\t", names = ["t_name_", "size","covered","sum","mean0","H3K4me1"])
+df2 = pd.read_csv(sys.argv[2],sep = "\t", names = ["t_name", "size","covered","sum","mean0","H3K4me3"])
+df3 = pd.read_csv(sys.argv[3],sep = "\t", names = ["t_name", "size","covered","sum","mean0","H3K9me3"])
+fpkm = pd.read_csv(sys.argv[4], sep = "\t")
+
+# print(fpkm)
+#fpkm = pd.read_csv(sys.argv[4], index_col = "t_id")
+
+# print(fpkm)
+
+mean1 = df1.loc[:,["t_name_","H3K4me1"]]
+mean2 = df2.loc[:,"H3K4me3"]
+mean3 = df3.loc[:,"H3K9me3"]
+fpkms = fpkm.loc[:,"FPKM"]
+lm = pd.concat((mean1,mean2,mean3,fpkms),axis = 1)
+# print(fpkms)
+print(lm)
+
+
+
+
+#print(lm)
+# print(mean)
+
+
+model = sm.formula.ols(formula = "FPKM ~ H3K4me1 + H3K9me3 + H3K4me3", data = lm)
+
+ols_results = model.fit()
+
+print(ols_results.summary())
+
